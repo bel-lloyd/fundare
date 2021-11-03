13ace05d3e4c6b625fdedca9dcfbfe5aaed3f875
Bel Lloyd <bel@zandiconsulting.com>
2021-11-03 19:36:02 +1000

    update to user admin

--- fundare/users/admin.py
+++ fundare/users/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from fundare.users.models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username']
+
 admin.site.register(CustomUser)
